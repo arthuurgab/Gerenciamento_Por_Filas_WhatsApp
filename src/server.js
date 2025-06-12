318e650888c19be061e26ebcbf9eac2839e638ae
arthuurgab <adarocha060@gmail.com>
2025-06-12 13:19:01 -0300

    logica de filas

--- src/server.js
+++ src/server.js
@@ -1,11 +1,8 @@
 const express = require('express');
-const { Queue } = require('bullmq');
-const axios = require('axios');
-
+const webhookQueue = require('./queue');
 const app = express();
 const port = 3333;
 
-const DATA_CRAZY_URL = 'https://api.datacrazy.io/v1/crm/api/crm/integrations/webhook/business/f8d6f7d8-2928-4514-b016-fceff8339a0f';
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -14,53 +11,16 @@ app.get('/', (req, res) => {
 });
 
 app.post('/webhook/filas', async (req, res) => {
-    const body = req.body;
-    let messageContextInfo = {};
-
-    dataSend = {
-        "event": body.event,
-        "instance": body.instance,
-        "data": {
-            "key": {
-                "remoteJid": body.data?.key?.remoteJid || '',
-                "fromMe": body.data?.key?.fromMe || false,
-                "id": body.data?.key?.id || ''
-            },
-            "name": body.data?.pushName,
-            "status": body.data?.status,
-            "message": {
-                "conversation": body.data?.message?.conversation || '',
-                "messageContextInfo": {
-                    "deviceListMetadata": {
-                        "senderKeyHash": body.data?.message?.messageContextInfo?.deviceListMetadata?.senderKeyHash,
-                        "senderTimestamp": body.data?.message?.messageContextInfo?.deviceListMetadata?.senderTimestamp,
-                        "recipientKeyHash": body.data?.message?.messageContextInfo?.deviceListMetadata?.recipientKeyHash,
-                        "recipientTimestamp": body.data?.message?.messageContextInfo?.deviceListMetadata?.recipientTimestamp,
-                    },
-                    "deviceListMetadataVersion": body.data?.message?.messageContextInfo?.deviceListMetadataVersion || '',
-                    "messageSecret": body.data?.message?.messageContextInfo?.messageSecret || '',
-                },
-            },       
-            "messageType": body.data?.messageType,
-            "messageTimestamp": body.data?.messageTimestamp,
-            "instanceId": body.data?.instanceId,
-            "source": body.data?.source
-        },
-        "destination": body.destination,
-        "date_time": body.date_time,
-        "sender": body.sender,
-        "server_url": body.server_url,
-        "apikey": body.apikey,
-       
-    }
     try {
-        const resposta = await axios.post(DATA_CRAZY_URL, dataSend, {
-            headers: { 'Content-Type': 'application/json' }
-        })
-        console.log("✔️ Enviando com sucesso para o Data Crazy", resposta.status);
+        await webhookQueue.add('enviarParaODataCrazy', req.body, {
+            attempts: 3,
+            backoff: 30000,
+        });
+        console.log("📥 Payload adicionado à fila");
         res.sendStatus(200);
-    } catch (err) {
-        console.log("❌ Erro ao enviar ao Data Crazy:", err.message)
+    } catch (error) {
+        console.error("❌ Erro ao adicionar na fila:", error.message);
+        res.sendStatus(500);
     }
 });
 
--- src/worker.js
+++ src/worker.js
@@ -1,19 +1,60 @@
-const {Worker} = require('bullmq');
+const { Worker } = require('bullmq');
+const axios = require('axios');
+const IORedis = require('ioredis'); 
+require('dotenv').config();
 
-const worker = new Worker('webhook-queue', async (job) => {
-    const webhookData = job.data;
-    console.log('Processando dados do webhook:', webhookData);
-}, {
-    connection: {
-        host: 'localhost',
-        port: 6379,
+const connection = new IORedis(process.env.REDIS_URL, {
+  maxRetriesPerRequest: null,
+});
+
+const worker = new Worker('webhookQueue', async (job) => {
+    const body = job.data;
+
+    const dataSend = {
+        "event": body.event,
+        "instance": body.instance,
+        "data": {
+            "key": {
+                "remoteJid": body.data?.key?.remoteJid || '',
+                "fromMe": body.data?.key?.fromMe || false,
+                "id": body.data?.key?.id || ''
+            },
+            "name": body.data?.pushName,
+            "status": body.data?.status,
+            "message": {
+                "conversation": body.data?.message?.conversation || '',
+                "messageContextInfo": {
+                    "deviceListMetadata": {
+                        "senderKeyHash": body.data?.message?.messageContextInfo?.deviceListMetadata?.senderKeyHash,
+                        "senderTimestamp": body.data?.message?.messageContextInfo?.deviceListMetadata?.senderTimestamp,
+                        "recipientKeyHash": body.data?.message?.messageContextInfo?.deviceListMetadata?.recipientKeyHash,
+                        "recipientTimestamp": body.data?.message?.messageContextInfo?.deviceListMetadata?.recipientTimestamp,
+                    },
+                    "deviceListMetadataVersion": body.data?.message?.messageContextInfo?.deviceListMetadataVersion || '',
+                    "messageSecret": body.data?.message?.messageContextInfo?.messageSecret || '',
+                },
+            },       
+            "messageType": body.data?.messageType,
+            "messageTimestamp": body.data?.messageTimestamp,
+            "instanceId": body.data?.instanceId,
+            "source": body.data?.source
+        },
+        "destination": body.destination,
+        "date_time": body.date_time,
+        "sender": body.sender,
+        "server_url": body.server_url,
+        "apikey": body.apikey,
+    };
+
+    try {
+        const response = await axios.post(process.env.DATA_CRAZY_URL, dataSend, {
+            headers: { 'Content-Type': 'application/json' }
+        });
+        console.log("✔️ Dados enviados com sucesso:", response.status);
+    } catch (err) {
+        console.error("❌ Falha ao enviar para o Data Crazy:", err.message);
     }
-});
-
-worker.on('completed', (job) => {
-    console.log(`Job ${job.id} concluído`);
-});
-
-worker.on('failed', (job, err) => {
-    console.log(`Job ${job.id} falhou com o erro ${err}`);
-});
+}, {
+    connection,
+    concurrency: 3,
+})
